ae89dd690c37a04e60f0d5a886a4fded606506b1
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-04-10 21:50:50 +0600

    split compose files to separate dev and test

---
+++ scripts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+local_up() {
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+}
+
+local_down() {
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml down
+}
+
+main() {
+    # Check if a function name was provided as an argument
+    if [ $# -eq 0 ]; then
+        echo "Usage: $0 <function_name>"
+        exit 1
+    fi
+
+    # Check if the provided function exists
+    if [ "$(type -t "$1")" = 'function' ]; then
+        # Execute the provided function
+        "$@"
+    else
+        echo "Function $1 doesn't exist."
+        exit 1
+    fi
+}
+
+main "$@"
