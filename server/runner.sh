c2c1327f8f2eb623a37a6609d43cb07902b80efc
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-04-10 18:07:52 +0600

    add docker compose

--- server/runner.sh
+++ server/runner.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 while ! nc -z "$DB_HOST" "$DB_PORT"; do
     echo "Waiting for DB to be available..."
     sleep 2
@@ -9,4 +7,5 @@ done
 
 echo "Starting server..."
 bun run db::migrate
-bun run src/index.ts &
+echo Done...
+bun run src/index.ts
