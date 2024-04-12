5bee8154ce9059834ae649c684b9ee3fd6ef2981
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-04-12 19:33:43 +0600

    use separate db params to form final url

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,7 +1,16 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres(process.env.DB_URL as string);
+const {
+  DB_HOST = "localhost",
+  DB_NAME = "mentorship-dev",
+  DB_USER = "postgres",
+  DB_PORT = 5432,
+  DB_PASSWORD = "password",
+} = process.env;
+
+const url = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const queryClient = postgres(url);
 const db = drizzle(queryClient);
 
 export { db };
