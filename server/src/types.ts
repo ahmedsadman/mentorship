c0d617cfbdfecf1b19d82aa7a24387e369bb00bb
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-05-24 15:25:04 +0600

    create mentee sessions

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,7 @@
 import type { mentee } from "./db/schemas/mentee";
+import type { session } from "./db/schemas/session";
 import type { user } from "./db/schemas/user";
 
 export type NewUser = typeof user.$inferInsert;
 export type NewMentee = typeof mentee.$inferInsert;
+export type NewSession = typeof session.$inferInsert;
