c6c1cdcba9fea43af9e15870f19f2bc87431cbcb
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-05-24 16:44:09 +0600

    add test for session creation

--- server/src/routes/TEST/mentee.test.ts
+++ server/src/routes/TEST/mentee.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, test } from "bun:test";
 import { db } from "@app/db";
 import { mentee } from "@app/db/schemas/mentee";
+import { session } from "@app/db/schemas/session";
 import { user } from "@app/db/schemas/user";
 import app from "@app/index";
+import menteeService from "@app/service/MenteeService";
 import { eq } from "drizzle-orm";
 
 describe("/mentee", () => {
@@ -50,6 +52,45 @@ describe("/mentee", () => {
     expect(dbUser[0]).not.toBeEmpty();
     expect(dbMentee[0]).not.toBeEmpty();
     expect(dbMentee[0].plan).toEqual("lite");
+    expect(await res.json()).toEqual(mockResp);
+  });
+
+  test("POST /mentee/:id/session", async () => {
+    const mentee = await menteeService.createMentee({
+      name: "Test",
+      email: "test@email.com",
+      password: "123",
+      plan: "lite",
+      price: 100,
+    });
+    const menteeId = mentee.mentee.id;
+
+    const bodyData = {
+      startTime: "2024-05-24T09:07:30.800Z",
+      endTime: "2024-05-24T09:07:45.800Z",
+    };
+
+    const res = await app.request(`/mentee/${menteeId}/session`, {
+      method: "POST",
+      body: JSON.stringify(bodyData),
+    });
+
+    const dbSession = await db
+      .select()
+      .from(session)
+      .where(eq(session.menteeId, menteeId));
+
+    expect(dbSession[0]).not.toBeEmpty();
+
+    const mockResp = {
+      id: dbSession[0].id,
+      menteeId,
+      startTime: bodyData.startTime,
+      endTime: bodyData.endTime,
+      status: "pending",
+    };
+
+    expect(res.status).toBe(201);
     expect(await res.json()).toEqual(mockResp);
   });
 });
