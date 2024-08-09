7f8caa253234d11c65afe760533bc9b8a800f131
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-08-09 16:06:06 +0600

    add sslmode for neon db provider

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -7,10 +7,17 @@ const {
   DB_USER = "postgres",
   DB_PORT = 5432,
   DB_PASSWORD = "password",
+  NODE_ENV = "dev",
 } = process.env;
 
-export const url = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export let url = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+if (NODE_ENV === "production") {
+  url = `${url}?sslmode=require`;
+}
+
 console.log("DB: ", url);
+
 const queryClient = postgres(url);
 const db = drizzle(queryClient);
 
