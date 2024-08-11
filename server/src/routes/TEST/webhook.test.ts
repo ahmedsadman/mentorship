57680a848d54bead6498e90febe24361eebaf2c6
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2024-08-12 00:28:50 +0600

    handle session cancel via webhook

--- server/src/routes/TEST/webhook.test.ts
+++ server/src/routes/TEST/webhook.test.ts
@@ -8,6 +8,7 @@ import menteeService, {
   type MenteeCreateResponse,
 } from "@app/service/MenteeService";
 import type { WebhookPayload } from "@app/service/MenteeService";
+import { WebhookTrigger } from "@app/types";
 import { eq } from "drizzle-orm";
 
 describe("/webhook", () => {
@@ -32,7 +33,10 @@ describe("/webhook", () => {
     });
   });
 
-  const createPayload = (email: string, overrides = {}): WebhookPayload => ({
+  const createPayload = (
+    email: string,
+    payloadOverrides = {},
+  ): WebhookPayload => ({
     triggerEvent: "BOOKING_CREATED",
     createdAt: "2024-05-19T07:43:51.464Z",
     payload: {
@@ -48,7 +52,7 @@ describe("/webhook", () => {
         },
       ],
       bookingId: null,
-      ...overrides,
+      ...payloadOverrides,
     },
   });
 
@@ -88,7 +92,7 @@ describe("/webhook", () => {
       expect(dbResult.user?.email).toEqual(mockMentee.user.email);
     };
 
-    test("Separate webhook sessions are handled properly", async () => {
+    test("Separate webhook sessions are handled properly for booking creation", async () => {
       const bodyData1 = createPayload(mockMentee1.user.email, { bookingId: 1 });
       const bodyData2 = createPayload(mockMentee2.user.email, { bookingId: 2 });
 
@@ -109,6 +113,49 @@ describe("/webhook", () => {
       expect(res2.status).toBe(200);
       const dbResult2 = await makeDBQuery(mockMentee2.user.email);
       assertSessionData(dbResult2[0], bodyData2, mockMentee2);
+    });
+
+    test("Webhooks without proper trigger event should not create/update data", async () => {
+      const bodyData = createPayload(mockMentee1.user.email, { bookingId: 1 });
+      bodyData.triggerEvent = "INVALID_TRIGGER";
+
+      const res = await app.request("/webhook/session", {
+        method: "POST",
+        body: JSON.stringify(bodyData),
+      });
+
+      expect(res.status).toBe(200);
+      const dbResult = await makeDBQuery(mockMentee1.user.email);
+      expect(dbResult).toBeEmpty();
+    });
+
+    test("Should cancel created session properly", async () => {
+      // -- Create the session via webhook --
+      const bodyData = createPayload(mockMentee1.user.email, {
+        bookingId: 110,
+      });
+      const createResp = await app.request("/webhook/session", {
+        method: "POST",
+        body: JSON.stringify(bodyData),
+      });
+      expect(createResp.status).toBe(200);
+
+      let dbResult = await makeDBQuery(mockMentee1.user.email);
+      expect(dbResult[0].session.status).toBe("accepted");
+
+      // -- Cancel the session via webhook --
+      const cancelBodyData = createPayload(mockMentee1.user.email, {
+        bookingId: 110,
+      });
+      cancelBodyData.triggerEvent = WebhookTrigger.BOOKING_CANCELLED;
+      const cancelResp = await app.request("/webhook/session", {
+        method: "POST",
+        body: JSON.stringify(cancelBodyData),
+      });
+      expect(cancelResp.status).toBe(200);
+
+      dbResult = await makeDBQuery(mockMentee1.user.email);
+      expect(dbResult[0].session.status).toBe("cancelled");
     });
   });
 });
